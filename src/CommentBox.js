c6185c22c1d23dfa6bb2bd5a9570da198888891d
Peter Wang <happypeter1983@gmail.com>
2017-08-03 22:12:00 +0800

    add two comments

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 class CommentBox extends Component {
+
+
+  state = {
+    comments: [
+      "hello1",
+      "hello2"
+    ]
+  }
+  
   render(){
+    let comStr = this.state.comments.map((item, index) => (
+      <li key={index}>{item}</li>
+    ))
     return(
       <div className="comment-box">
-        COmmentBox
+        <ul>
+          {comStr}
+        </ul>
       </div>
     )
   }
