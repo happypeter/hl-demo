5cb890dbb82116507e6342ee1d79663c8914e337
Peter Wang <happypeter1983@gmail.com>
2017-08-03 22:21:51 +0800

    comment with local state

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -9,16 +9,32 @@ class CommentBox extends Component {
       "hello2"
     ]
   }
-  
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      comments: [...this.state.comments, this.comInput.value ]
+    })
+    this.comInput.value = ''
+  }
+
   render(){
     let comStr = this.state.comments.map((item, index) => (
       <li key={index}>{item}</li>
     ))
+
+    let comForm = (
+      <form onSubmit={this.handleSubmit}>
+        <input ref={value => this.comInput = value} type="text" />
+        <button type="submit">提交</button>
+      </form>
+    )
     return(
       <div className="comment-box">
         <ul>
           {comStr}
         </ul>
+        {comForm}
       </div>
     )
   }
